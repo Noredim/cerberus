f172b3a2c03081c57372a1acb5a1043cb308df3c
Ricardo Noredim <ricardo.noredim@example.com>
2026-06-24 07:47:31 -0400

    fix(web-cache): bump SW version to 1.0.7 and configure Nginx to avoid caching index/SW and return 404 for missing assets

--- apps/web/public/service-worker.js
+++ apps/web/public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '1.0.6';
+const CACHE_VERSION = '1.0.7';
 const STATIC_CACHE_NAME = `cerberus-pwa-static-v${CACHE_VERSION}`;
 const DYNAMIC_CACHE_NAME = `cerberus-pwa-dynamic-v${CACHE_VERSION}`;
 
