87c2b0776c667b272a9b344b334317990f95bb28
Ricardo Noredim <ricardo.noredim@example.com>
2026-08-28 13:38:53 -0400

    build(web): retornar para arquitetura primaria com build estatico de producao na porta 80 e proxy_pass web:80

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -9,10 +9,6 @@ export default defineConfig({
     tailwindcss(),
   ],
   server: {
-    allowedHosts: true,
-    hmr: {
-      clientPort: 443,
-    },
     proxy: {
       '/uploads': {
         target: 'http://localhost:8000',
